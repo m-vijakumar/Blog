31d194774a25bf338833db63bf1e195cec4250c1
munikotivijaykumar <munikotivijaykumar@gmail.com>
2020-06-03 10:38:33 +0530

    final

--- client/src/index.js
+++ client/src/index.js
@@ -4,23 +4,6 @@ import './index.css';
 import App from './App';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-// import {createStore , applyMiddleware , combineReducers } from "redux" ;
-// import {Provider} from "react-redux";
-// import thunk from "redux-thunk";
-// import { composeWithDevTools } from "redux-devtools-extension";
-
-// import logReducer from "./store/reducers/LoginReducer"; 
-
-// const rootReducer = combineReducers({
-//   Log: logReducer
-// });
-
-// let store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(thunk))
-// )
-
-
 ReactDOM.render(
   // <Provider store={store}>
   //   <App />
